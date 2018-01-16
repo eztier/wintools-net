e74027255ce11f32a42c93e1edc8d6b8ac2ac36d
once-ler <htao@eztier.com>
2018-01-15 19:38:42 -0500

    Create separat Test project

---
+++ Test/Index.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace wintools.Test {
+
+  class Tester {
+    public static void Encryption(EncryptionProvider provider) {
+      var cipher_s = provider.Encrypt("192.168.1.22 2323 foo bar", Encoding.ASCII);
+
+      File.WriteAllText("credwin.txt", cipher_s + "\n", Encoding.ASCII);
+
+      string read_s = File.ReadAllText("credwin.txt", Encoding.ASCII);
+
+      string plaintext = provider.Decrypt(read_s, Encoding.ASCII);
+
+      Console.WriteLine(plaintext);
+    }
+  }
+
+  class Start {
+    static void Main(string[] args) {
+      var provider = new EncryptionProvider(wintools.Test.Properties.Settings.Default.privateKey, wintools.Test.Properties.Settings.Default.sharedSecret);
+
+      Tester.Encryption(provider);
+      InternalTest.EncryptionFromFile(provider);
+    }
+  }
+}
---
+++ Test/TestEncryption.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Linq;
+
+namespace wintools.Test {
+  class InternalTest {
+    public static void EncryptionFromFile(EncryptionProvider provider) {
+      var credstring = File.ReadAllText("resources\\security-plain.xml", Encoding.UTF8);
+      var cipher_s = provider.Encrypt(credstring, Encoding.UTF8);
+      File.WriteAllText("resources\\security-enc.xml", cipher_s, Encoding.UTF8);
+
+      string read_s = File.ReadAllText("resources\\security-enc.xml", Encoding.UTF8);
+      string plaintext = provider.Decrypt(read_s, Encoding.UTF8);
+
+      Console.WriteLine(plaintext);
+      // File.WriteAllText("resources\\security-restored.txt", plaintext + "\n", Encoding.UTF8);
+
+      var doc = XDocument.Parse(plaintext);
+      string module = "Click", store = "GRANTS", environment = "STAGING";
+
+      var el = doc.Descendants("Module").FirstOrDefault(e => e.Attribute("name").Value == module).Descendants("Store").FirstOrDefault(e => e.Attribute("name").Value == store).Descendants("Credential").FirstOrDefault(e => e.Attribute("environment").Value == environment);
+      var username = el.Attribute("username").Value;
+      var password = el.Attribute("password").Value;
+
+      module = "Epic";
+      el = doc.Descendants("Module").FirstOrDefault(e => e.Attribute("name").Value == module).Descendants("Credential").FirstOrDefault(e => e.Attribute("environment").Value == environment);
+      username = el.Attribute("username").Value;
+      password = el.Attribute("password").Value;
+
+      module = "OnBase";
+      el = doc.Descendants("Module").FirstOrDefault(e => e.Attribute("name").Value == module).Descendants("Credential").FirstOrDefault(e => e.Attribute("environment").Value == environment);
+      username = el.Attribute("username").Value;
+      password = el.Attribute("password").Value;
+    }
+  }
+}
